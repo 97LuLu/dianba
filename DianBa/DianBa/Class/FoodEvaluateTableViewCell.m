59d9600dcd150f4d49b8392a9f2c0464b723b09b
ldk <ldk@itcast.cn>
2017-01-04 09:11:15 +0800

    ldk 2017.1.4   9:11

---
+++ DianBa/DianBa/Class/FoodEvaluateTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  foodEvaluateTableViewCell.m
+//  DianBa
+//
+//  Created by Hunter on 2016/12/31.
+//  Copyright © 2016年 wangchenlu. All rights reserved.
+//
+
+#import "foodEvaluateTableViewCell.h"
+
+@implementation foodEvaluateTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
